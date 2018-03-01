2bb1807685952144d2781b874a721e1610f18664
Hendi Santika <hendisantika@gmail.com>
2018-03-01 07:09:46 +0700

    #9 - Add schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+
+-- Database Creation
+CREATE DATABASE burgers;
+USE burgers_db;
+
+
+-- Table Creation of Burgers
+CREATE TABLE burgers(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+burgerName VARCHAR(50),
+devoured BOOLEAN);
+
+
+-- Table Creation of Devourers
+CREATE TABLE devourers(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+devourerName VARCHAR(50),
+burgerId INTEGER);
+
+
+-- Seed Burgers into Database
+INSERT INTO burgers(burgerName, devoured)
+VALUES ("Big Mac", false),
+  ("Whooper", false),
+  ("Cheezburger", false),
+  ("Baconator", false),
+  ("Quarter Pounder", false),
+  ("McRoyal", false),
+  ("Double Double", true);
+
+
+-- Seed Devourers into Database
+INSERT INTO devourers(devourerName, burgerId)
+VALUES ("Tommy", 7);
