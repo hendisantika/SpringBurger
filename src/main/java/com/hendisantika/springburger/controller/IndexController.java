ece6cb5f5cac13624e0ab74a38e2711b386d9de7
hendisantika <hendisantika@yahoo.co.id>
2025-10-30 06:21:18 +0700

    feat: Add IndexController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springburger/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springburger.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : SpringBurger
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 30/10/25
+ * Time: 06.20
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class IndexController {
+
+    // Instance Variables (ivars)
+    private static String url;
+    private static String userName;
+    private static String password;
+
+}
